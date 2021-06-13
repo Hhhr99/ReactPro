1c26fae3f3a277e81594fac2246bf40ebc38123d
Hhhr <Hhhr1214@gmail.com>
2021-06-13 11:26:28 +0800

    添加一个脚手架项目

---
+++ day01/my-app/src/App.js
@@ -0,0 +1,14 @@
+import MyFirstReact from "./MyFirstReact.js";  // 导入组件
+// function App() {
+//     return(<div className="App">
+//         App内容
+//     </div>);
+// }
+function App() {
+    return (<div>
+        <MyFirstReact/>
+    </div>);
+}
+
+export default App;
+
---
+++ day01/my-app/src/MyFirstReact.js
@@ -0,0 +1,2 @@
+function MyFirstReact() {  return <div>我的第一个React组件</div>;  }// 编写组件代码，创建想要的界面元素
+export default MyFirstReact;// 导出组件
